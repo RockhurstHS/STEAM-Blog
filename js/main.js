a096b9322334147db54b3131dd8f0cffe26004a4
JamesWClark <jwclark@rockhursths.edu>
2015-08-31 00:39:53 -0500

    fix path in get for orgs

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ var setup = function() {
     $('#member-name').text(member.fullName);
   });
 
-  Trello.get("members/me/organizations/555f512347e116f185d698a4/boards", function(boards) {
+  Trello.get("organizations/555f512347e116f185d698a4/boards", function(boards) {
     boards.forEach(function(board) {
       console.log(board);
     });
